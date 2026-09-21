9760cfd420eb6da01d75c27acb13fa588802ca58
oluceps <secinia@outlook.com>
2026-09-21 12:22:55 +0800

    chore: clean examples

--- examples/test_pgp_fpr.rs
+++
@@ -1,9 +0,0 @@
-use pgp::composed::{Deserializable, SignedPublicKey};
-use pgp::types::KeyDetails;
-
-fn main() {
-    let key = SignedPublicKey::from_string("").unwrap().0;
-    let fpr = key.fingerprint();
-    let fpr_hex = hex::encode(fpr.as_bytes());
-    println!("{}", fpr_hex);
-}
